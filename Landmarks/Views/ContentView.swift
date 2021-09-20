2b15dc5f6b5bc7635a2ed6ba33a1946992a0e79a
Brandon Guzman <brandon.j.guzman.22@dartmouth.edu>
2021-09-19 21:41:47 -0400

    tutorial 3 done

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Brandon Guzman on 9/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
